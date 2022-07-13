7f0f7a1da359cd4d49ccb7c3e466b72751c799c4
Lautaro Rataric <lautarorataric163@gmail.com>
2022-07-13 07:05:13 -0300

    Modificando archivo forms.py de la app balanza. Seguir

--- src/apps/balanza/forms.py
+++ src/apps/balanza/forms.py
@@ -1,7 +1,11 @@
 from django     import forms
+
+from apps.padron.models import Padron
+#queryset=Padron.objects.all(),
 from .models    import Balanza
 
 class BalanzaForm(forms.ModelForm): 
+    padron = forms.ModelChoiceField(queryset=Padron.objects.all(),widget=forms.Select(attrs={'class':'form-control'}))
     class Meta:
         model = Balanza
         fields = ["padron", "codigo_productor", "tipo_alg", "establecimiento", "lotes", "variedad", "chofer", "cuit_chofer", "patente_camion", 
