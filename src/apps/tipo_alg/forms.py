7b1956f95bad25196ed475ae5e6b3091977a0fdc
Lautaro Rataric <lautarorataric163@gmail.com>
2022-07-14 01:34:29 -0300

    Modifique los templates de todas las apps.

--- src/apps/tipo_alg/forms.py
+++ src/apps/tipo_alg/forms.py
@@ -1,8 +1,14 @@
 from django     import forms
 from .models    import Tipo_Alg
 
+VARIEDAD_COSECHA = (
+    ("MANUAL", "MANUAL"), 
+    ("STRIPER", "STRIPER"), 
+    ("USILLO", "USILLO")
+)
+
 class Tipo_AlgForm(forms.ModelForm): 
-    descripcion_cosecha = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    descripcion_cosecha = forms.CharField(widget=forms.Select(choices=VARIEDAD_COSECHA, attrs={'class':'form-control'}))
     
     class Meta:
         model = Tipo_Alg
